59ca1bb1efce0d5331950ba4de9baf07497cd4fc
jylin@mba <jinyulin34@gmail.com>
2026-04-20 00:29:55 +0800

    [Add] MIT liscense

--- src/individual.hpp
+++ src/individual.hpp
@@ -14,10 +14,11 @@ public:
     Individual & operator=(Individual &&) = default; // Move Assignment Operator
     ~Individual() = default; // Destructor
     // Accessors
-    std::vector<double> const & genes() {return m_genes;} // genes getter 
+    std::vector<double> const & genes() const {return m_genes;} // genes getter 
     std::vector<double> & genes() {return m_genes;} // genes setter
-    double const & fitness() {return m_fitness;} // fitness getter
-    double & fitness() {return m_fitness;} // fitness setter 
+    double const & fitness() const {return m_fitness;} // fitness getter
+    double & fitness() {return m_fitness;} // fitness setter
+    // There may be more function to add, or custom constructor / destructor
 private:
     std::vector<double>  m_genes; // For private member data, we want a convention to distinguish them from other variables. Prefixing m_ is a common one. 
     double m_fitness = 0.0;
