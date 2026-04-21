8565365be375814226e9798b8bf6dad5868f3785
jylin@mba <jinyulin34@gmail.com>
2026-04-21 12:18:03 +0800

    [Add] basic unit test & integration test for later use

---
+++ tests/test_individual.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "individual.hpp"
+
+// 測試套件名稱：IndividualTest
+// 測試案例名稱：InitialAgeIsZero
+TEST(IndividualTest, InitialAgeIsZero) {
+    // 準備 (Arrange): 建立一個 Individual 物件
+    Individual ind("TestSubject");
+
+    // 斷言 (Assert): 驗證物件的初始年齡是否為 0
+    EXPECT_EQ(ind.fitness(), 0.0);
+}
